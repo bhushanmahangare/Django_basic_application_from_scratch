82b3ec6fb70289b43a548b29d094527ccf254c5e
Bhushan Mahangare <bhushan_mahangare@wifi-soft.com>
2020-06-10 21:21:08 +0530

    Added dynamic content in Template

--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Destination
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+
+    dest1 = Destination()   #create destination object
+    dest1.name = "Mumbai"
+    dest1.desc = "The city is newver "
+
+    return render(request,'index.html',{'dest1':dest1} )
+
+
+def about(request):
+    return render(request,'about.html')
